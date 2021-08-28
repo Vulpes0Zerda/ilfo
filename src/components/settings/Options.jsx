e04ddd285cf3bbadd98b13bd668b08426ebd9f7f
Lukas Burock <52589622+Vulpes0Zerda@users.noreply.github.com>
2021-08-28 19:40:04 +0200

    changed lang.long to .short

--- src/components/settings/Options.jsx
+++ src/components/settings/Options.jsx
@@ -9,7 +9,7 @@ const Options = ({ minLvl, maxLvl, currentLvl, lang }) => {
             >
                 {lang.map((language) => (
                     <option value={language.langCode} key={language.langCode}>
-                        {language.long}
+                        {language.short}
                     </option>
                 ))}
             </select>
@@ -39,7 +39,7 @@ const Options = ({ minLvl, maxLvl, currentLvl, lang }) => {
                 {
                     //TODO: need to overwork this (find solution for customizable checkbox)
                 }
-                <svg
+                {/* <svg
                     xmlns="http://www.w3.org/2000/svg"
                     xmlnsXlink="http://www.w3.org/1999/xlink"
                     width="30"
@@ -56,7 +56,7 @@ const Options = ({ minLvl, maxLvl, currentLvl, lang }) => {
                         d="M574.986 1210.41l635.344-635.287c8.66-8.658 18.62-12.987 29.87-12.987 11.26 0 21.22 4.329 29.87 12.987l68.83 68.831c9.53 9.524 14.08 19.697 13.64 30.52-.43 10.822-4.98 20.563-13.64 29.221L620.44 1422.1c-8.658 8.66-18.398 13.2-29.22 13.64-10.823.43-20.996-4.12-30.52-13.64l-404.309-404.24c-9.524-9.53-14.286-19.916-14.286-31.172 0-11.255 4.329-21.212 12.987-29.87l51.948-51.948c9.524-9.524 19.913-14.286 31.169-14.286 11.255 0 21.645 4.762 31.169 14.286l305.608 305.54z"
                         transform="translate(-142.105 -562.136)"
                     ></path>
-                </svg>
+                </svg> */}
             </label>
         </div>
     )
@@ -69,7 +69,7 @@ Options.defaultProps = {
     lang: [
         {
             langCode: 'EN',
-            short: 'ENG',
+            short: 'Eng',
             long: 'English',
         },
     ],
