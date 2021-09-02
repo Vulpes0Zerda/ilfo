941bdd42edfab8340daad708b32b9c2d06b8b452
Lukas Burock <52589622+Vulpes0Zerda@users.noreply.github.com>
2021-09-02 21:34:33 +0200

    trait tree header

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,4 +1,4 @@
-import Hunter from '../classes'
+import Beorning from '../classes/Beorning.json'
 
 const Content = () => {
     //TODO Programming logic for Trait tree for Left/Right Click [+/- Points to traits]
@@ -8,14 +8,70 @@ const Content = () => {
     return (
         <div className="content">
             <div className="tree__b tree">
-                <div className="tree__header">This is the header</div>
+                <div className="tree__header">
+                    <div className="tree__header__titel">
+                        <p>{Beorning.b.header.treeName}</p>
+                        <p>{`(${Beorning.b.header.treeRole})`}</p>
+                    </div>
+                    <div className="tree__header__content">
+                        <p>{Beorning.b.header.treeExplanation.short}</p>
+                        {Array.isArray(
+                            Beorning.b.header.treeExplanation.long,
+                        ) ? (
+                            Beorning.b.header.treeExplanation.long.map((t) =>
+                                t !== '' ? <p>{t}</p> : <br />,
+                            )
+                        ) : (
+                            <p>{Beorning.b.header.treeExplanation.long}</p>
+                        )}
+                        {Array.isArray(Beorning.b.header.treeInfo) ? (
+                            Beorning.b.header.treeInfo.map((t) =>
+                                t !== '' ? <p>{t}</p> : <br />,
+                            )
+                        ) : (
+                            <p>{Beorning.b.header.treeInfo}</p>
+                        )}
+                    </div>
+                </div>
                 <div className="tree__body">
                     <div className="tree__body__traits tree__body__traits--passiv">
-                        {lang.map((language) => (
-                            <div></div>
-                        ))}
+                        {Beorning.b.passiv.map((t) =>
+                            t.traitName ? (
+                                <div
+                                    className="tree__body__trait"
+                                    key={t.traitName}
+                                >
+                                    <div className="tree__body__trait__imgSizing">
+                                        <img
+                                            className="tree__body__trait__img"
+                                            src={t.traitPicture}
+                                            alt={t.traitName}
+                                        />
+                                    </div>
+                                    <div className="tree__body__trait__name">
+                                        {t.traitName}
+                                    </div>
+                                </div>
+                            ) : (
+                                ''
+                            ),
+                        )}
                     </div>
-                    <div className="tree__body__traits tree__body__traits--activ"></div>
+                    <div className="tree__body__traits tree__body__traits--activ">
+                        {Beorning.b.activ.map((t) =>
+                            t.traitName ? (
+                                <div className="tree__body__trait">
+                                    <img
+                                        src={t.traitPicture}
+                                        alt={t.traitName}
+                                    ></img>
+                                    <div>{t.traitName}</div>
+                                </div>
+                            ) : (
+                                ''
+                            ),
+                        )}
+                    </div>
                 </div>
             </div>
             <div className="tree__r tree"></div>
